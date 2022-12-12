f68d97c716a5c81b9b3947ecc0a4adcfa127a1fd
DiegoHerbas <dherbas@grupopaz.com.bo>
2022-12-12 00:27:53 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const suscriptionRouter = require("./routes/suscriptionRoute");
+const userRouter = require("./routes/userRoute");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.use("/suscription", suscriptionRouter);
+app.use("/user", userRouter);
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+
+  return;
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
