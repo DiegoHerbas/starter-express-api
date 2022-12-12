7afa0ef317b796c0e1579010c82382047a07a8c7
DiegoHerbas <dherbas@grupopaz.com.bo>
2022-12-12 11:03:55 -0400

    adición de script

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
+  // db: {
+  //   host: "34.68.124.223",
+  //   user: "biblioteca-sw2",
+  //   password: "biblioteca-sw2",
+  //   database: "biblioteca",
+  // },
   db: {
-    host: "34.68.124.223",
-    user: "biblioteca-sw2",
-    password: "biblioteca-sw2",
+    host: "localhost",
+    user: "root",
+    password: "",
     database: "biblioteca",
   },
-  // db: {
-  //   host: "localhost",
-  //   user: "root",
-  //   password: "",
-  //   database: "suscripcion",
-  // },
   listPerPage: 10,
 };
 
